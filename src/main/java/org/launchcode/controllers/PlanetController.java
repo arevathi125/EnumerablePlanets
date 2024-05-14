d773e6dd984f0337f7d08d9b3ca991ccd8db4450
revathi <arevathi125@gmail.com>
2024-05-14 12:07:51 -0500

    committed

--- src/main/java/org/launchcode/controllers/PlanetController.java
+++ src/main/java/org/launchcode/controllers/PlanetController.java
@@ -1,13 +1,33 @@
 package org.launchcode.controllers;
 
+import org.apache.tomcat.util.log.UserDataHelper;
+import org.launchcode.models.Planets;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class PlanetController {
 
     @GetMapping
-    public String displayIndex() {
+    public String displayIndex(Model model) {
+        model.addAttribute("planets", Planets.values());
         return "index";
     }
-}
+
+    @GetMapping("/planet")
+    public String displayPlanetInfo(@RequestParam("planet") String planet, Model model){
+           try {
+               Planets selectedPlanet = Planets.valueOf(planet.toUpperCase());
+               model.addAttribute("planet", selectedPlanet);
+               return "detail";
+           }
+           catch (IllegalArgumentException e){
+               return "redirect:/";
+        }
+
+           }
+
+    }
+
